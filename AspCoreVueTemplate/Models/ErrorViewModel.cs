6c70ed9db09385599dc11ebe58193b79e4fba00e
Hoang Quoc Nhat TONG <htong@expertime.com>
2018-06-21 09:43:21 +0200

    Add project files.

---
+++ AspCoreVueTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspCoreVueTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
